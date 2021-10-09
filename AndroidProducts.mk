8ffbc88209fabb8d6e56af169af5a169f84451f3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-09 15:23:47 +0200

    umi: Fluid bringup
    Change-Id: Ie8ef5c0a1a187c6109f27e0b933e6e4df426fd6d

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The fluidOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_umi.mk
+    $(LOCAL_DIR)/fluid_umi.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_umi-user \
-    lineage_umi-userdebug \
-    lineage_umi-eng
+    fluid_umi-user \
+    fluid_umi-userdebug \
+    fluid_umi-eng
