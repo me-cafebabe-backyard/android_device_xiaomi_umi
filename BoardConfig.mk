627b06cda8bf2a5067c60199b593dc319a6fc921
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-06 01:26:52 +0100

    umi: Add support for /dev/gx_fod_fp
    * 2 drivers can't register devices with the same name, so we must rename one of the two
    * Change FOD one to gx_fod_fp, as it's easy to sed in HAL blobs
    
    [cafebabe: Apply on device specific tree]
    
    Change-Id: I57d33cd055c060fdf4352f2bfb10d138b4db2f5a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,5 +27,8 @@ TARGET_OTA_ASSERT_DEVICE := umi
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Inherit from the proprietary version
 include vendor/xiaomi/umi/BoardConfigVendor.mk
